6525d3b5f1a14df143aac49aa5a7898db0a8f70b
royvayalathu <roy.vayalathu@gmail.com>
2017-02-28 12:28:46 +0100

    Update app.js

--- Node/core-MultiDialogs/app.js
+++ Node/core-MultiDialogs/app.js
@@ -15,19 +15,125 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
 server.post('/api/messages', connector.listen());
 
-var DialogLabels = {
+var address = null;
+var userSelection = null;
+var request = null;
+var response = null;
+
+server.get('/api/event', function(req, res) {
+
+    var newConversationAddress = Object.assign({}, address);
+    delete newConversationAddress.conversation;
+    request = req;
+    response = res;
+
+    bot.beginDialog(newConversationAddress, 'providerselection', null, (err) => {
+        if (err) {
+            // error ocurred while starting new conversation. Channel not supported?
+            bot.send(new builder.Message()
+                .text('This channel does not support this operation: ' + err.message)
+                .address(address));
+        }
+    });
+});
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    address = session.message.address;
+    // end current dialog
+    session.endDialog('Thank you for adding us to your contact list!');
+});
+
+var ChoiceLabels = {
     Yes: 'Yes',
-    No: 'No',
+    No: 'No'
 };
 
-var bot = new builder.UniversalBot(connector, [
+var SelectionLabels = {
+    Provider1: 'Service Provider 1',
+    Provider2: 'Service Provider 2',
+    Provider3: 'Service Provider 3'
+};
+
+
+bot.dialog('providerselection', [
+    function (session) {
+        builder.Prompts.choice(
+            session,
+            'Water Sensor in your Garage has detected water leak in your Garage. Would you like us to send some one to have a look?',
+            [ChoiceLabels.Yes, ChoiceLabels.No],
+            {
+                maxRetries: 3,
+                retryPrompt: 'Not a valid option'
+            });
+    },
+    function (session, result) {
+        if (!result.response) {
+            // exhausted attemps and no selection, start over
+            session.send('Ooops! Too many attemps :( But don\'t worry, I\'m handling that exception and you can try again!');
+            return session.endDialog();
+        }
+
+        // on error, start over
+        session.on('error', function (err) {
+            session.send('Failed with message: %s', err.message);
+            session.endDialog();
+        });
+
+        // continue on proper dialog
+        var selection = result.response.entity;
+        switch (selection) {
+            case ChoiceLabels.Yes:
+                session.userData.userChoice = 'yes';
+            case ChoiceLabels.No:
+                session.userData.userChoice = 'no';
+        }
+        session.send('Welcome to Enab.le Service Provider Selection!');
+        builder.Prompts.choice(
+            session,
+            'Please select your preferred Provider nearest to you',
+            [SelectionLabels.Provider1, SelectionLabels.Provider2, SelectionLabels.Provider3],
+            {
+                maxRetries: 3,
+                retryPrompt: 'Not a valid option'
+            });
+     },
+    function (session, result) {
+        if (!result.response) {
+            // exhausted attemps and no selection, start over
+            session.send('Ooops! Too many attemps :( But don\'t worry, I\'m handling that exception and you can try again!');
+            return session.endDialog();
+        }
+
+        // on error, start over
+        session.on('error', function (err) {
+            session.send('Failed with message: %s', err.message);
+            session.endDialog();
+        });
+
+        var selection = result.response.entity;
+        switch (selection) {
+            case SelectionLabels.Provider1:
+                session.userData.preferredProvider = 'provider1';
+            case SelectionLabels.Provider2:
+                session.userData.preferredProvider = 'provider2';
+            case SelectionLabels.Provider3:
+                session.userData.preferredProvider = 'provider3';
+        }
+        session.endDialog('We will soon notify the service provider to assist you ASAP %s', result.response);
+
+        response.json({requestedSupport: session.userData.userChoice,preferredProvider : session.userData.preferredProvider});
+   }
+]);
+
+/*var bot = new builder.UniversalBot(connector, [
     function (session) {
         // prompt for search option
         builder.Prompts.choice(
             session,
-            'The Water detector in your Garage has detected water in Garage. Shall we organize a support service for you?',
+            'Water Sensor in your Garage has detected water leak in your Garage. Would you like us to send some one to have a look?',
             [DialogLabels.Yes, DialogLabels.No],
             {
                 maxRetries: 3,
@@ -50,10 +156,10 @@ var bot = new builder.UniversalBot(connector, [
         // continue on proper dialog
         var selection = result.response.entity;
         switch (selection) {
-            case DialogLabels.No:
-                return session.beginDialog('flights');
             case DialogLabels.Yes:
                 return session.beginDialog('hotels');
+            case DialogLabels.No:
+                return session.beginDialog('flights');
         }
     }
 ]);
@@ -69,3 +175,5 @@ bot.dialog('support', require('./support'))
 bot.on('error', function (e) {
     console.log('And error ocurred', e);
 });
+
+*/
